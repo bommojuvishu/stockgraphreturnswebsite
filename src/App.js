9e6ee5d01f4b4fa5d10882914a01ae828a732fef
“bommojuvishu” <bommojuvishu@gmail.com>
2022-11-08 10:47:22 +0530

    added loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useEffect, useState}  from "react";
-import { Input, Button } from "antd";
+import { Input, Button , Spin} from "antd";
 import "antd/dist/antd.css";
 import LineChart from "./components/LineChart";
 import axios from "axios";
@@ -13,6 +13,7 @@ const baseURL = "https://7bsjtdr8bi.execute-api.ap-south-1.amazonaws.com/dev/";
 function App() {
   const  [closedata , setCloseData] = useState([])
   const [stocknames ,setStockNames] =  useState([])
+  const [loading, setLoading] = useState(false);
   let namesarr=[] 
   let  i = 0
 
@@ -25,12 +26,13 @@ function App() {
   };
 
   const reqGateway = () => {
-   
+    setLoading(true)
     axios
     .post(baseURL, {
       "name":namesarr
     })
     .then((response) => {
+      setLoading(false)
      setCloseData(response?.data.body)
      setStockNames( Object.keys(response?.data.body))
 
@@ -44,9 +46,10 @@ function App() {
         <h1>Stocks Returns</h1>
         <p></p>
       </div>
-      <Input placeholder="Basic usage" onChange={onInput} />
+      <Input placeholder="Enter stock symbol like INFY" onChange={onInput} allowClear={true}  />
       <Button type="primary" onClick={reqGateway}> Submit</Button>
-
+      <div> <Spin spinning={loading} size={"large"}></Spin></div>
+     
       { stocknames?.map((item , i ) => {
         i = i +1
         const tmp = closedata[item]
